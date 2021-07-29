601375d2095375a282d2a7eb2cc94490207f8cb2
Brad Keller <80160556+Bradkeller2117@users.noreply.github.com>
2021-07-29 19:59:29 -0400

    9.1

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const profileDataArgs = process.argv.slice(2);
+const { profile } = require("console");
 
-const printProfileData = profileDataArr => {
-    
-    for(let i = 0; i < profileDataArr.length; i +=1){
+const profileDataArgs = process.argv.slice(2,process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    for (let i = 0; i < profileDataArr.length; i++) {
         console.log(profileDataArr[i]);
     }
     console.log('================');
-
-   profileDataArr.forEach(profileItem => console.log(profileItem)); 
+   
+    // Is this the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+    
 };
-
 printProfileData(profileDataArgs);
